eef2841224c3d1fad39da932886103cb7f99c9b6
Jeroen van Lent <jeroen@vanlent.me>
2016-02-03 10:23:40 +0000

    Added breaks for proper testing

--- single.py
+++ single.py
@@ -3,6 +3,8 @@ import time
 
 times = 1
 
+print("Test: Request single file (regular requests)")
+raw_input("Press Enter to start testing HTTPv1")
 start1 = time.clock()
 for x in range(0, times):
   con1 = hyper.HTTP11Connection('iot.local', '1100')
@@ -10,6 +12,11 @@ for x in range(0, times):
   resp1 = con1.get_response()
 end1 = time.clock()
 
+print("HTTPv1")
+print(resp1.status)
+print(end1 - start1)
+
+raw_input("Press Enter to start testing HTTPv1 with SSL")
 start2 = time.clock()
 for x in range(0, times):
   con2 = hyper.HTTP11Connection('iot.local', '1200', 'True')
@@ -17,20 +24,17 @@ for x in range(0, times):
   resp2 = con2.get_response()
 end2 = time.clock()
 
+print("HTTPv1 with SSL")
+print(resp2.status)
+print(end2 - start2)
+
+raw_input("Press Enter to start testing HTTPv2 with SSL")
 start3 = time.clock()
 for x in range(0, times):
   con3 = hyper.HTTP20Connection('iot.local', '1300', 'True')
   con3.request('GET', '/')
   resp3 = con3.get_response()
 end3 = time.clock()
-
-print("HTTPv1")
-print(resp1.status)
-print(end1 - start1)
-
-print("HTTPv1 with SSL")
-print(resp2.status)
-print(end2 - start2)
 
 print("HTTPv2")
 print(resp3.status)
